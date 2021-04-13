dde94f79ac60d2d96c2ceaf04fcd28b5ca02d47e
NicoMilk <nicolas.coulot@gmail.com>
2021-04-13 15:26:36 +0200

    ADD react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import Home from './Components/Home';
+import Products from './Components/Products';
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  
+  <div className="App">
+    <Router>
+      <Navbar/>
+        <Route path='/' exact component={Home}/>
+        <Route path='/products' exact component={Products}/>
+        <Route path='/contact' exact component={Contact}/>
+    </Router>
+  </div>
+    );
 }
 
 export default App;
